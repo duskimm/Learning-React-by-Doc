8ad3e047e5bf827e77a6adf204ef1368b02dc42c
Douglas Toledo <Douglas Toledo - Duskimm>
2021-12-03 13:29:22 -0300

    Componentes Com State

--- training/src/App.js
+++ training/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ComponenteComState from './components/ComponenteComState/ComponenteComState';
 import ComponenteSimples from './components/componenteSimples/ComponenteSimples';
 
 function App() {
   return (
     <div>
       <ComponenteSimples name="Douglas" />
+      <ComponenteComState />
     </div>
   );
 }
